cab052e9d4a90b50aadb78d0099a3a0dc1cf0be1
Adrian Schneider <adrian.schneider@abusix.com>
2020-04-21 03:23:24 +0200

    initial commit

---
+++ assets/backgrounds/city.tsx
@@ -0,0 +1,259 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="city" tilewidth="32" tileheight="32" tilecount="85" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="13">
+  <image width="8" height="8" source="tiles/street-inner2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="8" height="8" source="tiles/street-inner1.png"/>
+ </tile>
+ <tile id="15">
+  <image width="8" height="8" source="tiles/street-inner4.png"/>
+ </tile>
+ <tile id="16">
+  <image width="8" height="8" source="tiles/street-inner3.png"/>
+ </tile>
+ <tile id="17">
+  <image width="8" height="8" source="tiles/crossing.png"/>
+ </tile>
+ <tile id="18">
+  <image width="8" height="8" source="tiles/street-outer.png"/>
+ </tile>
+ <tile id="19">
+  <image width="8" height="8" source="tiles/outer_path.png"/>
+ </tile>
+ <tile id="20">
+  <image width="8" height="8" source="tiles/shop-bottom-left.png"/>
+ </tile>
+ <tile id="21">
+  <image width="8" height="8" source="tiles/shop-bottom-center.png"/>
+ </tile>
+ <tile id="22">
+  <image width="8" height="8" source="tiles/shop-door-upper.png"/>
+ </tile>
+ <tile id="23">
+  <image width="8" height="8" source="tiles/shop-door-lower.png"/>
+ </tile>
+ <tile id="24">
+  <image width="16" height="8" source="tiles/toys-sign.png"/>
+ </tile>
+ <tile id="25">
+  <image width="8" height="8" source="tiles/shop-corner-plain.png"/>
+ </tile>
+ <tile id="26">
+  <image width="8" height="8" source="tiles/roof-lower-center.png"/>
+ </tile>
+ <tile id="27">
+  <image width="8" height="8" source="tiles/roof-lower-outer.png"/>
+ </tile>
+ <tile id="28">
+  <image width="8" height="8" source="tiles/shop-plain.png"/>
+ </tile>
+ <tile id="29">
+  <image width="8" height="8" source="tiles/shop-plain-center.png"/>
+ </tile>
+ <tile id="30">
+  <image width="8" height="8" source="tiles/shop-window.png"/>
+ </tile>
+ <tile id="31">
+  <image width="8" height="16" source="tiles/planter.png"/>
+ </tile>
+ <tile id="32">
+  <image width="8" height="8" source="tiles/shrubs.png"/>
+ </tile>
+ <tile id="33">
+  <image width="8" height="8" source="tiles/shrub2.png"/>
+ </tile>
+ <tile id="34">
+  <image width="8" height="8" source="tiles/flower.png"/>
+ </tile>
+ <tile id="35">
+  <image width="8" height="8" source="tiles/i.png"/>
+ </tile>
+ <tile id="36">
+  <image width="8" height="8" source="tiles/n.png"/>
+ </tile>
+ <tile id="37">
+  <image width="8" height="8" source="tiles/e.png"/>
+ </tile>
+ <tile id="38">
+  <image width="8" height="8" source="tiles/k.png"/>
+ </tile>
+ <tile id="39">
+  <image width="8" height="8" source="tiles/white-lines-center.png"/>
+ </tile>
+ <tile id="40">
+  <image width="8" height="8" source="tiles/white-lines-corner.png"/>
+ </tile>
+ <tile id="41">
+  <image width="8" height="8" source="tiles/white-center.png"/>
+ </tile>
+ <tile id="42">
+  <image width="8" height="8" source="tiles/white-corner.png"/>
+ </tile>
+ <tile id="43">
+  <image width="8" height="8" source="tiles/white-center-window.png"/>
+ </tile>
+ <tile id="44">
+  <image width="8" height="8" source="tiles/outer-parking2.png"/>
+ </tile>
+ <tile id="45">
+  <image width="8" height="8" source="tiles/outer-parking.png"/>
+ </tile>
+ <tile id="46">
+  <image width="8" height="8" source="tiles/street-inner-parking1.png"/>
+ </tile>
+ <tile id="47">
+  <image width="8" height="8" source="tiles/street-inner-parking2.png"/>
+ </tile>
+ <tile id="48">
+  <image width="8" height="8" source="tiles/generic-door-lower.png"/>
+ </tile>
+ <tile id="49">
+  <image width="8" height="8" source="tiles/generic-door-upper.png"/>
+ </tile>
+ <tile id="50">
+  <image width="8" height="8" source="tiles/tree-lower.png"/>
+ </tile>
+ <tile id="51">
+  <image width="8" height="8" source="tiles/tree-upper.png"/>
+ </tile>
+ <tile id="52">
+  <image width="8" height="16" source="tiles/pine.png"/>
+ </tile>
+ <tile id="53">
+  <image width="8" height="8" source="tiles/crack3.png"/>
+ </tile>
+ <tile id="54">
+  <image width="8" height="8" source="tiles/crack2.png"/>
+ </tile>
+ <tile id="55">
+  <image width="8" height="8" source="tiles/crack1.png"/>
+ </tile>
+ <tile id="56">
+  <image width="8" height="8" source="tiles/shop-plain-center-collapse.png"/>
+ </tile>
+ <tile id="57">
+  <image width="8" height="8" source="tiles/shop-plain-collapse-upper.png"/>
+ </tile>
+ <tile id="58">
+  <image width="8" height="8" source="tiles/shop-plain-collapse-lower.png"/>
+ </tile>
+ <tile id="59">
+  <image width="8" height="8" source="tiles/shop-bottom-center-broken.png"/>
+ </tile>
+ <tile id="60">
+  <image width="8" height="8" source="tiles/shop-bottom-left-broken.png"/>
+ </tile>
+ <tile id="61">
+  <image width="8" height="8" source="tiles/shop-window-broken.png"/>
+ </tile>
+ <tile id="62">
+  <image width="8" height="8" source="tiles/rubble.png"/>
+ </tile>
+ <tile id="64">
+  <image width="8" height="8" source="tiles/rubble2.png"/>
+ </tile>
+ <tile id="65">
+  <image width="8" height="8" source="tiles/roof-collapse.png"/>
+ </tile>
+ <tile id="66">
+  <image width="8" height="8" source="tiles/white-center-window-broken.png"/>
+ </tile>
+ <tile id="67">
+  <image width="8" height="8" source="tiles/Sprite-0037.png"/>
+ </tile>
+ <tile id="68">
+  <image width="8" height="8" source="tiles/antenna.png"/>
+ </tile>
+ <tile id="69">
+  <image width="16" height="8" source="tiles/mast-top-broken.png"/>
+ </tile>
+ <tile id="70">
+  <image width="16" height="8" source="tiles/mast-bottom-broken.png"/>
+ </tile>
+ <tile id="71">
+  <image width="8" height="8" source="tiles/collapse-outer.png"/>
+ </tile>
+ <tile id="72">
+  <image width="8" height="8" source="tiles/fault-beginning.png"/>
+ </tile>
+ <tile id="73">
+  <image width="8" height="8" source="tiles/fault_lower.png"/>
+ </tile>
+ <tile id="74">
+  <image width="8" height="8" source="tiles/fault-upper.png"/>
+ </tile>
+ <tile id="75">
+  <image width="8" height="8" source="tiles/watetr.png"/>
+ </tile>
+ <tile id="76">
+  <image width="8" height="8" source="tiles/bridge-railing.png"/>
+ </tile>
+ <tile id="77">
+  <image width="8" height="8" source="tiles/water-ledge2.png"/>
+ </tile>
+ <tile id="78">
+  <image width="8" height="8" source="tiles/water-ledge3.png"/>
+ </tile>
+ <tile id="79">
+  <image width="8" height="8" source="tiles/water-ledge1.png"/>
+ </tile>
+ <tile id="80">
+  <image width="8" height="8" source="tiles/brigdge-support.png"/>
+ </tile>
+ <tile id="81">
+  <image width="8" height="8" source="tiles/water-ledge-corner.png"/>
+ </tile>
+ <tile id="82">
+  <image width="8" height="8" source="tiles/vertical-double-window-shifted.png"/>
+ </tile>
+ <tile id="83">
+  <image width="8" height="8" source="tiles/vertical-double-window-ajar.png"/>
+ </tile>
+ <tile id="84">
+  <image width="8" height="8" source="tiles/vertical-double-window.png"/>
+ </tile>
+ <tile id="85">
+  <image width="8" height="8" source="tiles/vertical-double-window-broken.png"/>
+ </tile>
+ <tile id="86">
+  <image width="8" height="8" source="tiles/vertical-double-window-shifted-broken.png"/>
+ </tile>
+ <tile id="87">
+  <image width="8" height="8" source="tiles/street-inner-fault-start-lower.png"/>
+ </tile>
+ <tile id="88">
+  <image width="8" height="8" source="tiles/street-inner-fault-start.png"/>
+ </tile>
+ <tile id="89">
+  <image width="8" height="8" source="tiles/street-inner-fault-lower.png"/>
+ </tile>
+ <tile id="90">
+  <image width="8" height="8" source="tiles/street-inner-fault-upper.png"/>
+ </tile>
+ <tile id="91">
+  <image width="8" height="8" source="tiles/small-antenna.png"/>
+ </tile>
+ <tile id="92">
+  <image width="8" height="8" source="tiles/generic-door-upper-green.png"/>
+ </tile>
+ <tile id="93">
+  <image width="8" height="8" source="tiles/white-corner-window-broken.png"/>
+ </tile>
+ <tile id="94">
+  <image width="8" height="8" source="tiles/white-corner-window.png"/>
+ </tile>
+ <tile id="95">
+  <image width="8" height="8" source="tiles/white-narrow-window.png"/>
+ </tile>
+ <tile id="96">
+  <image width="8" height="8" source="tiles/white-narrow-window-broken.png"/>
+ </tile>
+ <tile id="97">
+  <image width="8" height="8" source="tiles/white-lines-narrow.png"/>
+ </tile>
+ <tile id="98">
+  <image width="32" height="32" source="tiles/car-in-hole.png"/>
+ </tile>
+</tileset>
---
+++ assets/backgrounds/inside.tsx
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="inside" tilewidth="16" tileheight="16" tilecount="42" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="8" height="8" source="tiles-inside/table-front.png"/>
+ </tile>
+ <tile id="1">
+  <image width="8" height="8" source="tiles-inside/table-center.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="tiles-inside/mixing-desk.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="tiles-inside/notes.png"/>
+ </tile>
+ <tile id="4">
+  <image width="8" height="8" source="tiles-inside/table-clear.png"/>
+ </tile>
+ <tile id="5">
+  <image width="8" height="8" source="tiles-inside/chair-upper.png"/>
+ </tile>
+ <tile id="6">
+  <image width="8" height="8" source="tiles-inside/chair-lower.png"/>
+ </tile>
+ <tile id="7">
+  <image width="8" height="8" source="tiles-inside/background-black.png"/>
+ </tile>
+ <tile id="8">
+  <image width="8" height="8" source="tiles-inside/floor-dark.png"/>
+ </tile>
+ <tile id="9">
+  <image width="8" height="8" source="tiles-inside/wall-straight.png"/>
+ </tile>
+ <tile id="10">
+  <image width="8" height="8" source="tiles-inside/wall-corner.png"/>
+ </tile>
+ <tile id="11">
+  <image width="8" height="8" source="tiles-inside/exit.png"/>
+ </tile>
+ <tile id="12">
+  <image width="8" height="8" source="tiles-inside/microphone-upper.png"/>
+ </tile>
+ <tile id="13">
+  <image width="8" height="8" source="tiles-inside/microphone-lower.png"/>
+ </tile>
+ <tile id="14">
+  <image width="8" height="8" source="tiles-inside/floor-light.png"/>
+ </tile>
+ <tile id="15">
+  <image width="8" height="8" source="tiles-inside/light-exit.png"/>
+ </tile>
+ <tile id="16">
+  <image width="8" height="8" source="tiles-inside/stairs.png"/>
+ </tile>
+ <tile id="17">
+  <image width="8" height="8" source="tiles-inside/stair-exit.png"/>
+ </tile>
+ <tile id="18">
+  <image width="8" height="8" source="tiles-inside/box.png"/>
+ </tile>
+ <tile id="19">
+  <image width="8" height="8" source="tiles-inside/cables.png"/>
+ </tile>
+ <tile id="21">
+  <image width="8" height="8" source="tiles-inside/wall-corner-inner.png"/>
+ </tile>
+ <tile id="22">
+  <image width="8" height="8" source="tiles/hole.png"/>
+ </tile>
+ <tile id="23">
+  <image width="8" height="8" source="tiles/inner-wall-collapse-upper.png"/>
+ </tile>
+ <tile id="24">
+  <image width="8" height="8" source="tiles/inner-wall-collapse-lower.png"/>
+ </tile>
+ <tile id="25">
+  <image width="8" height="8" source="tiles/table.png"/>
+ </tile>
+ <tile id="26">
+  <image width="8" height="8" source="tiles/chair.png"/>
+ </tile>
+ <tile id="27">
+  <image width="8" height="8" source="tiles/drawers.png"/>
+ </tile>
+ <tile id="28">
+  <image width="8" height="8" source="tiles/cupboard.png"/>
+ </tile>
+ <tile id="29">
+  <image width="8" height="8" source="tiles/worktop.png"/>
+ </tile>
+ <tile id="30">
+  <image width="8" height="8" source="tiles/worktop-inner.png"/>
+ </tile>
+ <tile id="31">
+  <image width="8" height="8" source="tiles/elevator-buttons.png"/>
+ </tile>
+ <tile id="32">
+  <image width="8" height="8" source="tiles/elevator-door-upper.png"/>
+ </tile>
+ <tile id="33">
+  <image width="8" height="8" source="tiles/elevator-door-lower.png"/>
+ </tile>
+ <tile id="34">
+  <image width="8" height="8" source="tiles/g.png"/>
+ </tile>
+ <tile id="35">
+  <image width="8" height="8" source="tiles/2.png"/>
+ </tile>
+ <tile id="36">
+  <image width="8" height="8" source="tiles/1.png"/>
+ </tile>
+ <tile id="37">
+  <image width="16" height="8" source="tiles/bed.png"/>
+ </tile>
+ <tile id="38">
+  <image width="8" height="8" source="tiles-inside/wall-straight-corner.png"/>
+ </tile>
+ <tile id="39">
+  <image width="8" height="8" source="tiles-inside/wal-innerl-corner-inner.png"/>
+ </tile>
+ <tile id="40">
+  <image width="8" height="8" source="tiles-inside/wall-inner-straight-corner.png"/>
+ </tile>
+ <tile id="41">
+  <image width="8" height="8" source="tiles-inside/wall-inner-straight.png"/>
+ </tile>
+ <tile id="42">
+  <image width="8" height="8" source="tiles-inside/3.png"/>
+ </tile>
+</tileset>
